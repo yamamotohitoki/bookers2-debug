0197dc36b095185c237a0bd2686d0deed763b290
EC2 Default User <ec2-user@ip-172-31-31-92.ap-northeast-1.compute.internal>
2023-08-03 03:45:29 +0000

    [Add] 非同期通信化

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,7 +11,7 @@ import "jquery"
 import "popper.js"
 import "bootstrap"
 
-import "../stylesheets/application" 
+import "../stylesheets/application"
 
 Rails.start()
 Turbolinks.start()
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
    resources :comments, only: [:create, :destroy]
-   resource :favorites, only: [:create, :destroy]
+   resources :favorites, only: [:create, :destroy]
   end
   resources :users, only: [:index,:show,:edit,:update] do
     resource :relationships, only: [:create, :destroy]
